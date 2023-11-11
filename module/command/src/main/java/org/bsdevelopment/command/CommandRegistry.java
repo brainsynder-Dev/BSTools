b3ca2431ade0ad7ca8383df2ebe9fcf36491879c
brainsynder <briansnyder498@gmail.com>
2023-11-10 20:43:39 -0500

    feat: Added a CommandRegistry.registerDirectly method to register commands without the plugin.yml

--- module/command/src/main/java/org/bsdevelopment/command/CommandRegistry.java
+++ module/command/src/main/java/org/bsdevelopment/command/CommandRegistry.java
@@ -1,10 +1,14 @@
 package org.bsdevelopment.command;
 
 import org.bsdevelopment.command.annotation.ICommand;
-import org.bukkit.command.PluginCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.List;
 
 public class CommandRegistry<P extends JavaPlugin> {
     private final P plugin;
@@ -34,5 +38,51 @@ public class CommandRegistry<P extends JavaPlugin> {
         command.setTabCompleter(commandCore);
         if (!annotation.description().isEmpty()) command.setDescription(annotation.description());
         if (annotation.alias().length != 0) command.setAliases(Arrays.asList(annotation.alias()));
+    }
+
+
+    public void registerDirectly(ParentCommand commandCore) throws Exception {
+        Class<?> clazz = commandCore.getClass();
+        if (!clazz.isAnnotationPresent(ICommand.class))
+            throw new Exception(clazz.getSimpleName() + " Missing ICommand.class");
+        ICommand annotation = clazz.getAnnotation(ICommand.class);
+        if (annotation.name().isEmpty()) throw new Exception(clazz.getSimpleName() + " missing name");
+
+        BasicCommand command = new BasicCommand(annotation, commandCore);
+        try {
+            final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+
+            bukkitCommandMap.setAccessible(true);
+            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+
+            commandMap.register(plugin.getName(), command);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class BasicCommand extends BukkitCommand implements TabCompleter {
+        private final ParentCommand commandCore;
+
+        private BasicCommand(ICommand command, ParentCommand commandCore) {
+            super(command.name());
+            if ((command.description() != null) && (!command.description().isEmpty())) setDescription(command.description());
+            if ((command.usage() != null) && (!command.usage().isEmpty())) setUsage(command.usage());
+            if (command.alias().length != 0) setAliases(Arrays.asList(command.alias()));
+
+            this.commandCore = commandCore;
+        }
+
+
+        @Override
+        public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+            commandCore.run(sender, args);
+            return false;
+        }
+
+        @Override
+        public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+            return commandCore.onTabComplete(sender, command, label, args);
+        }
     }
 }
